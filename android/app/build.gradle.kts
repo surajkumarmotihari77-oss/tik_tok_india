6579d721458607494e8bca8acb921618ca708bfb
surajkumarmotihari77-oss <surajkumarmotihari77@gmail.com>
2025-09-04 11:52:31 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,33 +1,33 @@
 plugins {
-    id "com.android.application"
-    id "kotlin-android"
-    id "com.google.gms.google-services"
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace "com.example.myapp"
-    compileSdkVersion 34
+    namespace = "com.suraj.tiktokindia"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.example.myapp"
-        minSdkVersion 21
-        targetSdkVersion 34
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.suraj.tiktokindia"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            signingConfig signingConfigs.debug
-            minifyEnabled false
-            shrinkResources false
+            signingConfig = signingConfigs.debug
+            isMinifyEnabled = false
+            shrinkResources = false
         }
     }
 }
 
 dependencies {
-    implementation "org.jetbrains.kotlin:kotlin-stdlib:1.9.10"
-    implementation platform('com.google.firebase:firebase-bom:33.1.2')
-    implementation 'com.google.firebase:firebase-auth'
-    implementation 'com.google.firebase:firebase-core'
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-core")
 }
